0e5aeae94f2c0ee8af0e0c92bad8e4a52faf44f7
Andrii <55098895+nnocturnnn@users.noreply.github.com>
2024-01-04 11:06:35 +0100

    feat: create db

--- cmflow/comments/admin.py
+++ cmflow/comments/admin.py
@@ -1,3 +1,19 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Attachment, Comment, Like
+
+
+class AttachmentInline(admin.TabularInline):
+    model = Attachment
+    extra = 1  # Number of extra forms to display
+
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = [
+        AttachmentInline,
+    ]
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Attachment)
+admin.site.register(Like)
